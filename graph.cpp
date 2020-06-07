d66d241a3acb8196e47a413659b2df87288e9b9f
Imbryczek <bartosz.bryk.lo@gmail.com>
2020-06-07 19:10:22 +0200

    Klasa graph

---
+++ graph.cpp
@@ -0,0 +1,102 @@
+#include "graph.h"
+
+EdgeNode::EdgeNode() {
+	this->number = 0;
+	this->weight = 0;
+	this->next =   NULL;
+}
+
+EdgeNode::EdgeNode(int number, int weight) {
+	this->number = number;
+	this->weight = weight;
+	this->next =   NULL;
+}
+
+
+EdgeNode::~EdgeNode(){}
+
+Graph::Graph(bool directed, int nodeNumber) {
+
+	this->directed = directed;
+	this->nodeNumber = nodeNumber;
+
+	for (int i = 1; i < this->nodeNumber + 1; i++)
+		this->edges[i] = NULL;	
+}
+
+Graph::~Graph() {}
+
+void Graph::InsertEdge(int x, int y, int weight, bool directed) {
+	if (x > 0 && x < (this->nodeNumber + 1) && y>0 && y < (this->nodeNumber + 1)) {
+
+		EdgeNode* edge = new EdgeNode(y, weight);
+		edge->next = this->edges[x];
+		this->edges[x] = edge;
+		if (!directed)
+			insertEdge(y, x, weight, true);
+	}
+}
+
+void Graph::print() {
+	for (int v = 1; v < this->nodeNumber + 1; v++) {
+		if (this->edges[v] != NULL) {
+			std::cout << "Vertex" << v << " has neighbours: " << endl;
+			EdgeNode* current = this->edges[v];
+			while (current != NULL) {
+				std::cout << current->number << endl;
+				current = current->next;
+				}
+		}
+	}
+}
+
+void InitVars(bool discovered[], int distance[], int parent[], int graphSize) {
+	for (int i = 1; i < graphSize + 1; i++) {
+		discovered[i] = false;
+		distance[i] = std::numeric_limits<int>::max();
+		parent[i] = -1;
+	}
+}
+
+void DijkstraAlgorithm(Graph* g, int parent[], int distance[], int start) {
+
+	bool discovered[g->number + 1];
+	EdgeNode* tmp;
+
+	int vTMP;
+	int vNeighbour;
+	int weight;
+	int smallestDistance;
+
+	InitVars(discovered, distance, parent, g->number);
+
+	while (discovered[vTMP] == false) {
+		discovered[vTMP] == true;
+		tmp = g->edges[vTMP];
+
+		while (tmp!=NULL){
+			vNeighbour = tmp->number;
+			weight = tmp->weight;
+			if ((distance[vTMP] + weight) < distance[vNeighbour]) {
+				distance[vNeighbour] = distance[vTMP] + weight;
+				parent[vNeighbour] = vTMP;
+			}
+			tmp = tmp->next;
+		}
+
+		smallestDistance = std::numeric_limits<int>::max();
+		for (int i = 1; i < (g->number); i++) {
+			if (!discovered[i] && (distance[i] < smallestDistance)) {
+				vTMP = i;
+				smallestDistance = distance[i];
+			}
+		}
+	}
+}
+
+void PrintShortestPath(int v, int parent[], int graphNumber) {
+	if (v > 0 && (v < graphNumber + 1) && parent[v] != -1) {
+		std::cout << parent[v] << " ";
+		PrintShortestPath(parent[v], parent, graphNumber)
+	}
+}
---
+++ graph.h
@@ -0,0 +1,64 @@
+/**
+  @file graph.h
+  @brief Implementation graph in C++.
+  @version 1.0
+  @date 2020-06-05
+  @author Jakub Brzezowski
+*/
+
+#pragma once
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <limits>
+
+/**
+  @class EdgeNote
+  @brief Impelementation of basics elements of graphs in combination simplified usage in graph class. 
+  @version 1.0
+  @date 2020-06-05
+  @author Jakub Brzezowski
+*/
+class EdgeNode {
+	public:
+		int number;				/*!<Number of node*/
+		int weight;				/*!<Weight of edge*/
+		EdgeNode* next;			/*!<Pointer to next node, which represents edge*/
+
+
+		EdgeNode();
+		EdgeNode(int, int);
+
+		~EdgeNode();
+};
+
+/**
+  @class Graph
+  @brief Impelementation of mathematical graph
+  @version 1.0
+  @date 2020-06-05
+  @author Jakub Brzezowski
+*/
+class Graph {
+
+	private:
+		bool directed;
+		const int nodeNumber;
+		EdgeNode *edges[nodeNumber + 1];	/*!< Container with edges and nodes*/
+	public:
+
+		Graph(bool,int);
+		~Graph();
+
+		void InsertEdge(int, int, int, bool);
+		void print();
+
+};
+
+void InitVars(bool discovered[], int distance[], int parent[], int graphSize);
+void DijkstraAlgorithm(Graph* g, int parent[], int distance[], int start);
+
+void PrintShortestPath(int v, int parent[], int graphNumber);
+
+#endif // !GRAPH_H
